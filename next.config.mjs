f4db52b06d81a7c1e5f9dc9258c57da64907b53b
naimul-creator <naimul.stu@gmail.com>
2024-09-28 18:18:19 +0600

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['res.cloudinary.com', 'hostname'], // একাধিক hostname যুক্ত করুন
+    },
+  };
+  
+  export default nextConfig;
+  
---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomePage from './products/page'
+
+const page = () => {
+  return (
+   <>
+   <HomePage />
+   </>
+  )
+}
+
+export default page
